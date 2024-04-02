13f530e90497e1849132e3f35f24d71c4ae0b890
Skyler Ruiter <ruitersk@mail.gvsu.edu>
2024-04-01 21:44:14 -0400

    matrix multiplication??

--- src/ivsparse.cpp
+++ src/ivsparse.cpp
@@ -6,6 +6,8 @@ using namespace Rcpp;
 class BaseVCSC {
  public:
   virtual ~BaseVCSC() {}
+  virtual double coeff(int i, int j) = 0;
+  virtual NumericMatrix mult(NumericMatrix dense_mat) = 0;
 };
 
 // Templated derived class
@@ -13,6 +15,29 @@ template <typename T, typename U>
 class DerivedVCSC : public BaseVCSC {
  public:
   IVSparse::VCSC<T, U> *mat;
+
+  double coeff(int i, int j) {
+    return mat->coeff(i, j);
+  }
+
+  NumericMatrix mult(NumericMatrix dense_mat) {
+    // make a new dense matrix to store the result
+    NumericMatrix result(dense_mat.ncol(), dense_mat.nrow());
+
+    // transpose mat
+    NumericMatrix mat_t = transpose(dense_mat);
+
+    int outerDim = mat->cols();
+
+    for (uint32_t i = 0; i < outerDim; ++i) {
+      for (typename IVSparse::VCSC<T, U>::InnerIterator it(*mat, i); it; ++it) {
+        result.column(it.getIndex()) = result.column(it.getIndex()) + (mat_t.column(i) * it.value());
+      }
+    }
+
+    // return result transposed
+    return transpose(result);
+  }
 };
 
 // Your VCSC class
@@ -23,19 +48,76 @@ class VCSC {
     Rcout << "VCSC() called" << std::endl;
   }
 
+  VCSC(const S4 &mat) {
+    // create the vectors
+    IntegerVector i, p, Dim;
+    NumericVector x;
+    List Dimnames;
+
+    // check if the matrix is a dgCMatrix
+    if (mat.inherits("dgCMatrix")) {
+      i = mat.slot("i");
+      p = mat.slot("p");
+      x = mat.slot("x");
+      Dim = mat.slot("Dim");
+    } else {
+      stop("Invalid matrix type");
+    }
+
+    uint64_t nrow = Dim[0];
+    uint64_t ncol = Dim[1];
+    uint64_t nnz = i.size();
+
+    // get pointers to the vectors
+    int *i_ptr = &i[0];
+    int *p_ptr = &p[0];
+    double *x_ptr = &x[0];
+
+    // initialize the VCSC matrix
+    DerivedVCSC<double, int> *derived_vcsc_mat = new DerivedVCSC<double, int>();
+
+    derived_vcsc_mat->mat = new IVSparse::VCSC<double, int>(x_ptr, i_ptr, p_ptr, nrow, ncol, nnz);
+
+    // init vcsc_mat
+    vcsc_mat = derived_vcsc_mat;
+    
+    // print the matrix
+    for (int i = 0; i < ncol; i++) {
+      for (int j = 0; j < nrow; j++) {
+        Rcout << derived_vcsc_mat->mat->coeff(i, j) << " ";
+      }
+      Rcout << std::endl;
+    }
+  }
+
   VCSC(const S4 &mat, std::string data_type, std::string index_type) {
+
+    // create the vectors
+    IntegerVector i, p, Dim;
+    NumericVector x;
+    List Dimnames;
+
+    // check if the matrix is a dgCMatrix
+    if (mat.inherits("dgCMatrix")) {
+      i = mat.slot("i");
+      p = mat.slot("p");
+      x = mat.slot("x");
+      Dim = mat.slot("Dim");
+    } else {
+      stop("Invalid matrix type");
+    }
+
+    uint64_t nrow = Dim[0];
+    uint64_t ncol = Dim[1];
+    uint64_t nnz = i.size();
+
+    // get pointers to the vectors
+    int *i_ptr = &i[0];
+    int *p_ptr = &p[0];
+    double *x_ptr = &x[0];
+
     if (data_type == "int" && index_type == "int") {
       DerivedVCSC<int, int> *derived_vcsc_mat = new DerivedVCSC<int, int>();
-
-      // initialize the VCSC matrix
-      IntegerVector dims = mat.slot("Dim");
-      IntegerVector i = mat.slot("i");
-      IntegerVector p = mat.slot("p");
-      IntegerVector x = mat.slot("x");
-
-      int nrow = dims[0];
-      int ncol = dims[1];
-      int nnz = i.size();
 
       // get the data from the i, p, and x slots into a std::vector
       std::vector<int> i2(i.begin(), i.end());     // indices
@@ -50,25 +132,42 @@ class VCSC {
       // initialize the VCSC matrix
       derived_vcsc_mat->mat = new IVSparse::VCSC<int, int>(x2_ptr, i2_ptr, p2_ptr, nrow, ncol, nnz);
 
-      // Assign the derived_vcsc_mat to vcsc_mat
+      // init vcsc_mat
       vcsc_mat = derived_vcsc_mat;
 
       // print nnz using nonZeros() vcsc method
       int nnz_vcsc = derived_vcsc_mat->mat->nonZeros();
       Rcout << "nnz_vcsc: " << nnz_vcsc << std::endl;
-    }
-    // Add more conditions for other data types
 
-  }
-
-  VCSC(double x) {
-    Rcout << "VCSC(double x) called" << std::endl;
+      // iterate through the vcsc matrix and print each vlaue
+      for (int i = 0; i < ncol; i++) {
+        for (int j = 0; j < nrow; j++) {
+          Rcout << derived_vcsc_mat->mat->coeff(i, j) << " ";
+        }
+        Rcout << std::endl;
+      }
+    } 
+    // else if (data_type == "" && index_type == "") {
+    //   /* code */
+    // } else if (data_type == "" && index_type == "") {
+    //   /* code */
+    // } else if (data_type == "" && index_type == "") {
+    //   /* code */
+    // } else {
+    //   stop("Invalid data_type or index_type");
+    // }
   }
 
   ~VCSC() { delete vcsc_mat; }
 
   // Add methods here
+  double coeff(int i, int j) {
+    return vcsc_mat->coeff(i, j);
+  }
 
+  NumericMatrix mult(NumericMatrix dense_mat) {
+    return vcsc_mat->mult(dense_mat);
+  }
 
 
  private:
@@ -79,8 +178,10 @@ class VCSC {
 RCPP_MODULE(vcsc) {
   class_<VCSC>("VCSC")
     .constructor()
+    .constructor<S4>()
     .constructor<S4, std::string, std::string>()
-    .constructor<double>()
     // Add methods here
+    .method("coeff", &VCSC::coeff)
+    .method("mult", &VCSC::mult)
     ;
 }
