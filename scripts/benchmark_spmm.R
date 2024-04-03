8aa157e07e0b5e3e25372d033f04361ccef299dc
Skyler Ruiter <ruitersk@mail.gvsu.edu>
2024-04-03 19:52:20 -0400

    updates benchmark file

--- scripts/benchmark_spmm.R
+++ scripts/benchmark_spmm.R
@@ -4,16 +4,25 @@ library(Matrix)
 library(microbenchmark)
 
 n <- 5000
+low <- 10
+high <- 20
 
-dense <- matrix(rnorm(n*n), ncol=n) # dense matrix of doubles
+
+dense <- matrix(rnorm(n * n), ncol = n) # dense matrix of doubles
 
 sparse <- rsparsematrix(n, n, 0.1) # random (mostly unique) matrix of doubles
-
 vcsc1 <- VCSC$new(sparse) # use doubles and int indices
 
-orig_spmm <- function(sparse, dense) {sparse %*% dense}
+sparse_redundant <- rsparsematrix(n, n, 0.1, rand.x = function(n) rpois(2, 1) + 1)
+vcsc2 <- VCSC$new(sparse_redundant, "int", "int")
 
-new_spmm <- function(sparse, dense) {sparse$mult(dense)}
+orig_spmm <- function(sparse, dense) {
+  sparse %*% dense
+}
+
+new_spmm <- function(sparse, dense) {
+  sparse$mult(dense)
+}
 
 bench <- microbenchmark(
   orig_spmm(sparse, dense),
@@ -21,4 +30,14 @@ bench <- microbenchmark(
   times = 3
 )
 
+bench2 <- microbenchmark(
+  orig_spmm(sparse_redundant, dense),
+  new_spmm(vcsc2, dense),
+  times = 3
+)
+
+print("Unique Sparse")
 print(bench)
+
+print("\n 50% redundant Sparse")
+print(bench2)
